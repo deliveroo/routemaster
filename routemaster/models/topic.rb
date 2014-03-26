95fa1429cff859c6e50bf46708be1bcfe7c10640
Julien Letessier <julien.letessier@gmail.com>
2014-03-26 18:41:57 +0000

    Progresses through Topic spec

--- routemaster/models/topic.rb
+++ routemaster/models/topic.rb
@@ -14,14 +14,37 @@ class Routemaster::Models::Topic < Routemaster::Models::Base
       conn.hget(_key, 'publisher') == @publisher
   end
 
+  def publisher
+    @publisher
+  end
+
+  def subscribers
+    conn.smembers("#{_key}/subscribers")
+  end
+
+  def push(event:, url:)
+    event = Event.new(event, url)
+    conn.rpush(_key_events, event.dump)
+  end
+
+  def peek
+    raw_event = conn.lindex(_key_events, 0)
+    return if raw_event.nil?
+    Event.load(raw_event)
+  end
+
   private
 
   def _key
     @_key ||= 'channels/#{@name}'
   end
 
+  def _key_events
+    @_key_events ||= "#{_key}/events"
+  end
+
   class Name < String
-    def new(str)
+    def initialize(str)
       raise ArgumentError unless str.kind_of?(String)
       raise ArgumentError unless str =~ /[a-z_]{1,32}/
       super
@@ -29,10 +52,37 @@ class Routemaster::Models::Topic < Routemaster::Models::Base
   end
 
   class Publisher < String
-    def new(str)
+    def initialize(str)
       raise ArgumentError unless str.kind_of?(String)
       raise ArgumentError unless str =~ /[a-z0-9:_-]{1,64}/
       super
     end
   end
+
+  class Event
+    VALID_TYPES = %w(create update delete noop)
+    LOAD_REGEXP = Regexp.new("^(?<type>#{VALID_TYPES.join('|')}),(?<url>.*)$")
+
+    def initialize(type, url)
+      raise ArgumentError.new('bad event type') unless VALID_TYPES.include?(type)
+      parsed_url = URI.parse(url)
+      raise ArgumentError.new('entity URL is not HTTPS') unless parsed_url.scheme == 'https'
+      raise ArgumentError.new('entity URL has query string') unless parsed_url.query.nil?
+      @_type = type
+      @_url = url
+    end
+
+    def dump
+      "#{@_type},#{@_url}"
+    end
+
+    def type ; @_type ; end
+    def url  ; @_url  ; end
+
+    def self.load(string)
+      return unless match = LOAD_REGEXP.match(string)
+      new(match['type'], match['url'])
+    end
+  end
 end
+
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,6 +3,8 @@ require 'spec/support/models.rb'
 require 'routemaster/models/topic'
 
 describe Routemaster::Models::Topic do
+  subject { described_class.new(name: 'widgets', publisher: 'bob') }
+
   describe '.new' do
     it 'fails wihtout arguments' do
       expect {
@@ -24,27 +26,69 @@ describe Routemaster::Models::Topic do
     end
   end
 
+
   describe '#publisher' do
-    it 'returns the channel publisher'
+    it 'returns the channel publisher' do
+      expect(subject.publisher).to eq('bob')
+    end
+
   end
+
 
   describe '#subscribers' do
-    it 'returns the list of channel subscribers'
+    it 'returns the list of channel subscribers' do
+      expect(subject.subscribers).to eq([])
+    end
   end
+
 
   describe '#push' do
-    it 'succeeds with correct parameters'
-    it 'fails with incorrect event types'
-    it 'fails with a bad URL'
+    it 'succeeds with correct parameters' do
+      expect {
+        subject.push(event: 'create', url: 'https://example.com/widgets/123')
+      }.not_to raise_error
+    end
+
+    it 'fails with incorrect event types' do
+      expect {
+        subject.push(event: 'whatever', url: 'https://example.com/widgets/123')
+      }.to raise_error(ArgumentError)
+    end
+
+    it 'fails with a non-HTTPS URL' do
+      expect {
+        subject.push(event: 'create', url: 'http://example.com/widgets/123')
+      }.to raise_error(ArgumentError)
+    end
+
+    it 'fails if the URL has a query string' do
+      expect {
+        subject.push(event: 'create', url: 'https://example.com/widgets/123?wut')
+      }.to raise_error(ArgumentError)
+    end
   end
 
+
   describe '#peek' do
-    it 'returns the oldest event for the topic'
+    it 'returns nil if the topic has no events' do
+      expect(subject.peek).to be_nil
+    end
+
+    it 'returns the oldest event for the topic' do
+      subject.push(event: 'create', url: 'https://example.com/widgets/123')
+      event = subject.peek
+      expect(event.type).to eq('create')
+      expect(event.url).to  eq('https://example.com/widgets/123')
+    end
+
+    it 'adds timestamps'  # should be an Event spec
   end
+
 
   describe '#pop' do
     it 'discards the oldest event for the topic'
   end
+
 
   describe '#events'
 end
