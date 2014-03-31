b659fee10a16828c6df7cfd8824b97d554ad83e0
Julien Letessier <julien.letessier@gmail.com>
2014-03-31 10:52:41 +0200

    Cleans up persistence spec helper

--- spec/support/persistence.rb
+++ spec/support/persistence.rb
@@ -1,11 +1,16 @@
 
-require 'routemaster/models/base'
-class RedisCleaner < Routemaster::Models::Base
+require 'routemaster/mixins/connection'
+require 'singleton'
+
+class RedisCleaner
+  include Singleton
+  include Routemaster::Mixins::Connection
+
   def clean!
     conn.flushdb
   end
 end
 
 RSpec.configure do |config|
-  config.after(:each) { RedisCleaner.new.clean! }
+  config.before(:each) { RedisCleaner.instance.clean! }
 end
