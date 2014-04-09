c814c82f74afd44fb5f4ecd27543c6216e0cb560
Julien Letessier <julien.letessier@gmail.com>
2014-04-09 13:28:41 +0100

    Adds pending spec for the application wrapper

---
+++ spec/application_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'routemaster/application'
+
+describe Routemaster::Application do
+  describe 'unknown endpoint' do
+    it 'responds with an error, no content'
+  end
+end
