919e93a1259bb438e42df705bc8a5f6cb98b5b49
Julien Letessier <julien.letessier@gmail.com>
2014-04-14 00:37:29 +0100

    Adds watch wrapper

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: bundle exec unicorn -c config/unicorn.rb
+watch: bundle exec ruby -I. -r watch.rb
 
---
+++ watch.rb
@@ -0,0 +1,17 @@
+require 'routemaster/services/watch'
+require 'routemaster/mixins/log'
+include Routemaster::Mixins::Log
+
+_log.info { 'creating watch' }
+watch = Routemaster::Services::Watch.new
+
+_log.info { 'trapping signals for clean exit' }
+%w(INT TERM QUIT).each do |signal|
+  Kernel.trap(signal) { Thread.new { watch.stop } }
+end
+
+_log.info { 'running watch' }
+watch.run
+_log.info { 'watch completed' }
+Kernel.exit(0)
+
