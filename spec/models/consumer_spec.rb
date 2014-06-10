47f3fc9a0006eef39e819d4a9bf2440f872a7be2
Julien Letessier <julien.letessier@gmail.com>
2014-06-10 17:36:01 +0200

    Refactors & specs Consumer

---
+++ spec/models/consumer_spec.rb
@@ -0,0 +1,102 @@
+require 'spec_helper'
+require 'spec/support/persistence'
+require 'routemaster/models/consumer'
+require 'routemaster/models/subscription'
+
+describe Routemaster::Models::Consumer do
+  class Receiver
+    def initialize
+      @messages = 0
+    end
+
+    def on_message(message)
+      @messages += 1
+    end
+
+    def on_cancel
+    end
+
+    def wait_for_message(count: 1, timeout: 1.0)
+      started_at    = Time.now
+      until (Time.now > started_at + timeout) || (@messages >= count)
+        sleep 10e-3
+      end
+    end
+  end
+
+  let(:subscription) {
+    Routemaster::Models::Subscription.new(subscriber: 'alice')
+  }
+
+  let(:receiver) { Receiver.new }
+
+  let(:options) {{
+    subscription: subscription,
+    on_message:   receiver.method(:on_message),
+    on_cancel:    receiver.method(:on_cancel)
+  }}
+
+  subject { described_class.new(**options) }
+
+
+  describe '#initialize' do
+    it 'passes with valid args' do
+      expect { subject }.not_to raise_error
+    end
+
+    it 'requires subscription:' do
+      options.delete :subscription
+      expect { subject }.to raise_error(ArgumentError)
+    end
+
+    it 'requires on_cancel:' do
+      options.delete :on_cancel
+      expect { subject }.to raise_error(ArgumentError)
+    end
+
+    it 'requires on_message:' do
+      options.delete :on_message
+      expect { subject }.to raise_error(ArgumentError)
+    end
+  end
+
+
+  describe '#start' do
+    it 'starts message delivery' do
+      subscription.queue.publish('kill')
+      expect(receiver).to receive(:on_message)
+      subject.start
+      receiver.wait_for_message
+    end
+
+    it 'delivers multiple messages' do
+      10.times { subscription.queue.publish('kill') }
+      expect(receiver).to receive(:on_message).exactly(10).times
+      subject.start
+      receiver.wait_for_message(count: 10)
+    end
+
+    it 'can be called twice' do
+      2.times { subject.start }
+    end
+  end
+
+  describe '#stop' do
+    it 'stops message delivery' do
+      expect(receiver).not_to receive(:on_message)
+      subject.start
+      subject.stop
+      receiver.wait_for_message
+      subscription.queue.publish('kill')
+    end
+
+    it 'can be called twice' do
+      subject.start
+      2.times { subject.stop }
+    end
+
+    it 'can be called without starting' do
+      subject.stop
+    end
+  end
+end
