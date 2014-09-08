7eb50b84b8f347b0f028d808447519b2a8fed36b
Marcus Mitchell <marcusleemitchell@gmail.com>
2014-09-08 13:30:30 +0100

    platform/add-datadog-monitoring-alerts-77614326 require deliver metric and iterate over available sunscriptions

--- routemaster/services/scheduler.rb
+++ routemaster/services/scheduler.rb
@@ -1,5 +1,6 @@
 require 'rufus-scheduler'
-require 'routemaster/services/scheduler.rb'
+require 'routemaster/services/scheduler'
+require 'routemaster/services/deliver_metric'
 
 scheduler = Rufus::Scheduler.new
 
@@ -8,16 +9,18 @@ scheduler.every '1m' do
     "env:#{ENV['RACK_ENV']}",
     'app:routemaster'
   ]
+  
+  Routemaster::Models::Subscription.each do |subscription|
+    Routemaster::Services::DeliverMetric.deliver(
+      'subscription.queue.size',
+      subscription.queue.message_count,
+      tags
+    )
 
-  Routemaster::Services::DeliverMetric.deliver(
-    'subscription.queue.size',
-    subscription.queue.message_count,
-    tags
-  )
-
-  Routemaster::Services::DeliverMetric.deliver(
-    'subscription.queue.staleness',
-    subscription.queue.age_of_oldest_message,
-    tags
-  )
+    Routemaster::Services::DeliverMetric.deliver(
+      'subscription.queue.staleness',
+      subscription.queue.age_of_oldest_message,
+      tags
+    )
+  end
 end
