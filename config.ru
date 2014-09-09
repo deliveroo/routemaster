24ab8b914bd26e174802fc1b6a880022f56b77fb
Marcus Mitchell <marcusleemitchell@gmail.com>
2014-09-09 10:15:08 +0100

    [#77614326] added env var to controll scheduling

--- config.ru
+++ config.ru
@@ -2,8 +2,7 @@ require 'dotenv'
 require 'config/openssl'
 require 'core_ext/string'
 require 'routemaster/application'
-require 'newrelic_rpm'
-
+require 'routemaster/services/scheduler'
 require 'newrelic_rpm' if ENV['NEW_RELIC_LICENSE_KEY']
 
 Dotenv.load!
--- routemaster/services/scheduler.rb
+++ routemaster/services/scheduler.rb
@@ -4,26 +4,31 @@ require 'routemaster/services/deliver_metric'
 
 include Routemaster::Services::DeliverMetric
 
-scheduler = Rufus::Scheduler.new
+if ENV.fetch('SCHEDULER_ENABLED', false)
+  puts "SCHEDULER_ENABLED"
+  scheduler = Rufus::Scheduler.new
 
-scheduler.every '1s' do
+  scheduler.every '5s' do
+    tags = [
+      "env:#{ENV['RACK_ENV']}",
+      'app:routemaster'
+    ]
 
-  tags = [
-    "env:#{ENV['RACK_ENV']}",
-    'app:routemaster'
-  ]
+    Routemaster::Models::Subscription.each do |subscription|
+      Routemaster::Services::DeliverMetric.deliver(
+        'subscription.queue.size',
+        subscription.queue.message_count,
+        tags + ["subscription:#{subscription.subscriber}"]
+      )
 
-  Routemaster::Models::Subscription.each do |subscription|
-    Routemaster::Services::DeliverMetric.deliver(
-      'subscription.queue.size',
-      subscription.queue.message_count,
-      tags << "subscription:#{subscription.subscriber}"
-    )
-
-    Routemaster::Services::DeliverMetric.deliver(
-      'subscription.queue.staleness',
-      subscription.age_of_oldest_message,
-      tags << "subscription:#{subscription.subscriber}"
-    )
+      Routemaster::Services::DeliverMetric.deliver(
+        'subscription.queue.staleness',
+        subscription.age_of_oldest_message,
+        tags + ["subscription:#{subscription.subscriber}"]
+      )
+    end
   end
+
+else
+  puts "SCHEDULER_DISABLED"
 end
