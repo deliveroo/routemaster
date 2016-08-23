51606eb5f3c1853b5b52b1bbba9825ec0f86a83b
Julien Letessier <julien.letessier@gmail.com>
2016-08-23 17:25:50 +0200

    Fixes missing logging

--- routemaster/controllers/topics.rb
+++ routemaster/controllers/topics.rb
@@ -1,12 +1,15 @@
 require 'routemaster/controllers'
 require 'routemaster/models/topic'
 require 'routemaster/services/ingest'
+require 'routemaster/mixins/log'
 require 'sinatra'
 require 'json'
 
 module Routemaster
   module Controllers
     class Topics < Sinatra::Base
+      include Routemaster::Mixins::Log
+
       get '/topics' do
         content_type :json
         Routemaster::Models::Topic.all.map do |topic|
@@ -49,6 +52,8 @@ module Routemaster
             timestamp: event_data['timestamp'] || Routemaster.now
           )
         rescue ArgumentError => e
+          _log.warn { "failed to parse event" }
+          _log_exception(e)
           halt 400, 'bad event'
         end
 
