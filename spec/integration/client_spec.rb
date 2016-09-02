6ab5c08cc2c1ae711f2081d78f8b2d5c99608c9d
Julien Letessier <julien.letessier@gmail.com>
2016-09-02 16:45:28 +0200

    Fixes client integration specs

--- spec/integration/client_spec.rb
+++ spec/integration/client_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'spec/support/integration'
 require 'routemaster/client'
-require 'routemaster/models/subscription'
+require 'routemaster/models/subscriber'
 require 'routemaster/models/topic'
 
 describe 'Client integration' do
@@ -19,9 +19,9 @@ describe 'Client integration' do
   after  { client_processes.each { |c| c.stop } }
 
   let(:client) { Routemaster::Client.new(url: 'https://127.0.0.1:17893', uuid: 'demo') }
-  let(:subscription) { Routemaster::Models::Subscription.find('demo') }
+  let(:subscriber) { Routemaster::Models::Subscriber.find('demo') }
   let(:topic) { Routemaster::Models::Topic.find('widgets') }
-  let(:queue) { subscription.queue }
+  let(:queue) { subscriber.queue }
 
   it 'connects' do
     expect { client }.not_to raise_error
@@ -36,8 +36,8 @@ describe 'Client integration' do
       callback: 'https://127.0.0.1:17894/events'
     )
 
-    expect(subscription).not_to be_nil
-    expect(subscription.callback).to eq('https://127.0.0.1:17894/events')
+    expect(subscriber).not_to be_nil
+    expect(subscriber.callback).to eq('https://127.0.0.1:17894/events')
   end
 
   it 'publishes' do
@@ -47,7 +47,7 @@ describe 'Client integration' do
     expect(topic.get_count).to eq(1)
   end
 
-  context 'with a subscription' do
+  context 'with a subscriber' do
     before do
       # implicitly create the topic first
       client.created('widgets', 'https://example.com/widgets/1')
@@ -60,12 +60,12 @@ describe 'Client integration' do
 
     it 'unsubscribes from a topic' do
       client.unsubscribe('widgets')
-      expect(subscription.topics.map(&:name)).not_to include('widgets')
+      expect(subscriber.topics.map(&:name)).not_to include('widgets')
     end
     
     it 'unsubscribes entirely' do
       client.unsubscribe_all
-      expect(subscription).to be_nil
+      expect(subscriber).to be_nil
     end
 
     it 'enqueues' do
