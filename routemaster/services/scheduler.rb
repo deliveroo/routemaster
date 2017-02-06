a9d0f41158ab17b37cd5179793d625e5250e3be0
Julien Letessier <julien.letessier@gmail.com>
2017-02-06 17:23:15 +0100

    IP-466: Out-of-band job promotion
    If promotion of scheduled jobs to instant is itself a job, and
    scrubbing is a job too, there’s a risk of a failed state where a
    “schedule” job is pending, but can’t be scrubbed because the
    “scub_queues” job can’t be promoted.

--- routemaster/services/scheduler.rb
+++ routemaster/services/scheduler.rb
@@ -1,18 +1,27 @@
-require 'routemaster/jobs'
+require 'routemaster/services'
 require 'routemaster/models/queue'
 require 'routemaster/mixins/log'
 
 module Routemaster
-  module Jobs
-    class Schedule
+  module Services
+    class Scheduler
       include Mixins::Log
 
       def call
+        next_at = Routemaster.now + INTERVAL
+
         Models::Queue.each do |q|
           jobs = q.schedule
           _log.debug { "scheduler: promoted #{jobs} jobs on queue.#{q.name}" }
         end
+
+        sleep TICK while (block_given? ? yield : true) && Routemaster.now < next_at
       end
+
+      private
+
+      TICK = 10e-3
+      INTERVAL = 100 # schedule jobs every X ms
     end
   end
 end
