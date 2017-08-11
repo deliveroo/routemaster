f622fc1df406eb071fb85c2da198759b64be2745
Julien Letessier <julien.letessier@gmail.com>
2017-08-11 15:16:51 +0200

    Fixes Redis connection issues

--- config/bootstrap.rb
+++ config/bootstrap.rb
@@ -32,15 +32,3 @@ if ENV['NEW_RELIC_LICENSE_KEY']
     add_transaction_tracer :perform, :category => :task
   end
 end
-
-if ENV['ROUTEMASTER_CLIENTS']
-  require 'routemaster/mixins/redis'
-  require 'routemaster/mixins/log'
-  require 'routemaster/models/client_token'
-  $stderr.puts 'Warning: Migrating $ROUTEMASTER_CLIENTS to redis storage. Support adding clients via environment variable is deprecated. See README.'
-
-  ENV.fetch('ROUTEMASTER_CLIENTS', '').split(',').each do |token|
-    name = token.sub(/--.*/, '')
-    Routemaster::Models::ClientToken.create!(name: name, token: token)
-  end
-end
