bbea40e995346715680bc92cd53f4df724ef4bf3
Julien Letessier <julien.letessier@gmail.com>
2017-09-08 10:25:50 +0200

    Adds validation spec

--- routemaster/models/user.rb
+++ routemaster/models/user.rb
@@ -7,8 +7,8 @@ module Routemaster
       include Mixins::Assert
 
       def initialize(str)
-        _assert str.kind_of?(String)
-        _assert(str =~ /\A[a-z0-9:_-]{1,64}\Z/)
+        _assert str.kind_of?(String), 'requires a string'
+        _assert(str =~ /\A[a-z0-9:_-]{1,64}\z/, 'contains invalid characters')
         super
       end
     end
