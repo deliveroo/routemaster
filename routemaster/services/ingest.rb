17299277e8f6dbed7625b1b21d68a153e9a810e1
Gurkan Oluc <gurkanoluc@gmail.com>
2017-12-13 15:27:59 +0000

    Add newrelic tracking to ingestion endpoint (#107)
    * Add Newrelic tracing for publish endpoint
    
    * Wrap promote with tracing
    
    * Guard tracing queue#promote with if stmt

--- routemaster/services/ingest.rb
+++ routemaster/services/ingest.rb
@@ -1,9 +1,11 @@
 require 'routemaster/services'
 require 'routemaster/mixins/assert'
 require 'routemaster/mixins/counters'
+require 'routemaster/mixins/newrelic'
 require 'routemaster/models/batch'
 require 'routemaster/models/message'
 require 'routemaster/models/job'
+
 
 module Routemaster
   module Services
@@ -12,6 +14,7 @@ module Routemaster
     class Ingest
       include Mixins::Assert
       include Mixins::Counters
+      include Mixins::Newrelic
 
       def initialize(topic:, event:, queue:)
         _assert(event.topic == topic.name)
@@ -21,18 +24,38 @@ module Routemaster
       end
 
       def call
-        data = Services::Codec.new.dump(@event)
-        @topic.subscribers.each do |s|
-          batch = Models::Batch.ingest(data: data, timestamp: @event.timestamp, subscriber: s)
+        trace_with_newrelic('Custom/Services/ingest') do
+          data = Services::Codec.new.dump(@event)
+          @topic.subscribers.each do |s|
+            trace_with_newrelic("Custom/Services/ingest-#{s.name}") do
+              batch = nil
 
-          job = Models::Job.new(name: 'batch', args: batch.uid, run_at: batch.deadline)
-          @queue.push(job)
-          @queue.promote(job) if batch.full?
+              trace_with_newrelic('Custom/Services/batch-ingest') do
+                batch = Models::Batch.ingest(data: data, timestamp: @event.timestamp, subscriber: s)
+              end
+
+              job = Models::Job.new(name: 'batch', args: batch.uid, run_at: batch.deadline)
+
+              trace_with_newrelic('Custom/Services/queue-push') do
+                @queue.push(job)
+              end
+
+              if batch.full?
+                trace_with_newrelic('Custom/Services/queue-promote') do
+                  @queue.promote(job)
+                end
+              end
+            end
+          end
+
+          _counters.incr('events.published', topic: @topic.name)
+          _counters.incr('events.bytes', topic: @topic.name, count: data.length)
+
+          trace_with_newrelic('Custom/Services/topic-increment') do
+            @topic.increment_count
+          end
         end
 
-        _counters.incr('events.published', topic: @topic.name)
-        _counters.incr('events.bytes', topic: @topic.name, count: data.length)
-        @topic.increment_count
         self
       end
     end
