6454e8ef0961c47160b5e69c6b65f43e549bdff7
Joel van Velden <joel@tpnz.co.nz>
2018-09-11 17:48:57 +0100

    routemaster_cleanup (#132)
    * remove scripts used for migration

--- bin/run_with_elasticache_redis.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# due to having two routemasters in one hopper app we need a way to
-# set the redis url differently for some of them. this is how we do it.
-set -e
-
-export ROUTEMASTER_REDIS_URL=$ELASTICACHE_ROUTEMASTER_REDIS_URL
-$@
